42ace975f4269db2314020ab20114db946198814
Daniel Stonier <d.stonier@gmail.com>
2014-07-23 01:40:13 +0900

    Don't write messages twice
    This fixes #4.

--- scripts/rename_topic.py
+++ scripts/rename_topic.py
@@ -4,5 +4,9 @@ from rosbag import Bag
 
 with Bag('correctodom_wide_flat.bag', 'w') as bag:
     for topic, msg, t in Bag('original.bag'):
-        bag.write('/image_left' if topic == '/image_left4' else topic, msg, t)
-        bag.write('/image_right' if topic == '/image_right4' else topic, msg, t)
+        if topic == '/image_left4':
+            bag.write('image_left', msg, t)
+        elif topic == '/image_right4':
+            bag.write('image_right', msg, t)
+        else:
+            bag.write(topic, msg, t)
